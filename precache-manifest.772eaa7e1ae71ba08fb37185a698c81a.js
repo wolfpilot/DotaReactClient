82230c8a6d27f5e15601c8b71b65b9b40ea0a569
Razvan Negrea <howl@wolfpilot.co.uk>
2019-04-08 01:00:58 +0200

    Updates

---
+++ precache-manifest.772eaa7e1ae71ba08fb37185a698c81a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a42d9f54d86870afcf9",
+    "url": "/DotaReactClient/static/js/runtime~main.8e5a3ab8.js"
+  },
+  {
+    "revision": "c6545ba4a4808345583c",
+    "url": "/DotaReactClient/static/js/main.e613636b.chunk.js"
+  },
+  {
+    "revision": "d9ae0c4545e56d4fc2ab",
+    "url": "/DotaReactClient/static/js/2.d36c43b7.chunk.js"
+  },
+  {
+    "revision": "c6545ba4a4808345583c",
+    "url": "/DotaReactClient/static/css/main.95c44bfa.chunk.css"
+  },
+  {
+    "revision": "d9ae0c4545e56d4fc2ab",
+    "url": "/DotaReactClient/static/css/2.10535824.chunk.css"
+  },
+  {
+    "revision": "46907379e732a26a90eafe8aa1b15234",
+    "url": "/DotaReactClient/index.html"
+  }
+];
